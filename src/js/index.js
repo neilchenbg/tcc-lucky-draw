621ceca0ef8353c9ca53dc4ed2e7a6e5b944805f
Lance Cheng <lcheng@campuscruiser.com>
2017-01-18 16:43:36 +0800

    加入 Deviceready

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,21 @@
-require(
-  ['jquery', 'view/app'],
-  function ($, ViewApp) {
-    "use strict";
+if (window.cordova) {
+  document.addEventListener("deviceready", function() {
+    require(
+      ['jquery', 'view/app'],
+      function ($, ViewApp) {
+        "use strict";
 
-    var AppView = new ViewApp({el: $('#app')});
-  }
-);
+        var AppView = new ViewApp({el: $('#app')});
+      }
+    );
+  }, false);
+} else {
+  require(
+    ['jquery', 'view/app'],
+    function ($, ViewApp) {
+      "use strict";
+
+      var AppView = new ViewApp({el: $('#app')});
+    }
+  );
+}
